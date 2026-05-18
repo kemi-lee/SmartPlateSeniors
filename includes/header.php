053e37520cb91f7e6d0bcf45359322ca1fe172f1
kemi-lee <eunicedigitals64@gmail.com>
2026-05-18 13:28:32 -0400

    fix nutrition explorer link

--- includes/header.php
+++ includes/header.php
@@ -89,7 +89,7 @@ $initial    = strtoupper(substr($username, 0, 1) ?: 'U');
                             <svg width="12" height="12" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2.5"><polyline points="6 9 12 15 18 9"/></svg>
                         </button>
                         <div class="dropdown-menu">
-                            <a href="/Nutrition Explore Page/nutrition-explorer.php">
+                            <a href="/nutrition-explorer/nutrition-explorer.php">
                                 <span>🔍</span> Explore
                             </a>
                             <a href="/PHP/recipe_generator.php">
